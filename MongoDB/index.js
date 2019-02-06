1f40f3fa31f54cac5ca574c2d7c8713808719bec
LazyKing <bhaskar3797mishra@gmail.com>
2019-02-06 20:38:46 +0530

    Adding Lesson - CRUD Operation with Mongoose

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -29,50 +29,110 @@ async function createCourse() {
   console.log(result);
 }
 
-async function getCourses() {
-  const pageNumber = 2;
-  const pageSize = 10;
+// async function getCourses() {
+//   const pageNumber = 2;
+//   const pageSize = 10;
 
-  const courses
+//eq(equal)
+//ne(not equal)
+//gt(greater Than)
+//gte(greater than or equal to)
+//lt(less than)
+//lte(less than or equal to)
+//in
+//nin(not in)
 
-  //eq(equal)
-  //ne(not equal)
-  //gt(greater Than)
-  //gte(greater than or equal to)
-  //lt(less than)
-  //lte(less than or equal to)
-  //in
-  //nin(not in)
+//Logical Operators
+//or
+//and
 
-  //Logical Operators
-  //or
-  //and
+// const courses = await Course
+// .find({ author: "Mosh", isPublished: true )}
+// .find({ price: 10 })
+// .find({ price: { $gt: 10, $lte: 20 } })
+// .find({ price: { $in: [10, 15, 20] } })
+// .find()
+// .or([{ author: "Mosh"} , { isPublished: true }])
+// .and([{}])
 
-  const courses = await Course
-    // .find({ author: "Mosh", isPublished: true )}
-    // .find({ price: 10 })
-    // .find({ price: { $gt: 10, $lte: 20 } })
-    // .find({ price: { $in: [10, 15, 20] } })
-    // .find()
-    // .or([{ author: "Mosh"} , { isPublished: true }])
-    // .and([{}])
+//author that name starts with mosh
+//  .find({ author: /^Mosh/ })
 
-    //author that name starts with mosh
-    //  .find({ author: /^Mosh/ })
+//Ends with Hamedani
+// .find({ author: /Hamedani$/i }) //i for case insesitive
 
-    //Ends with Hamedani
-    // .find({ author: /Hamedani$/i }) //i for case insesitive
+//Contains Mosh
+// .find({author: /.*Mosh*/i })
 
-    //Contains Mosh
-    // .find({author: /.*Mosh*/i })
+//For Pagination
+//     .skip((pageNumber - 1) * pageSize)
+//     .limit(pageSize)
+//     .sort({ name: 1 })
+//     // .select({ name: 1, tags: 1 });
+//     .count();
+//   console.log(courses);
+// }
 
-    //For Pagination 
-    .skip((pageNumber - 1) * pageSize)
-    .limit(pageSize)
-    .sort({ name: 1 })
-    // .select({ name: 1, tags: 1 });
-    .count();
-  console.log(courses);
+// async function updateCourse(id) {
+// Approach : Query first
+// findbyId()
+//Modify its properties
+//save()
+
+// const course = await Course.findById(id);
+// if (!course) return;
+// course.isPublished = true;
+// course.author = "Another Author";
+
+// course.set({
+//   isPublished: true,
+//   author: "Another Author"
+//});
+// const result = await course.save();
+// console.log(result);
+
+//Approach : Update first
+//Update directly
+//Optionallly: get the updated Document
+
+// const course = await Course.update(
+//   { _id: id },
+//   {
+//     $set: {
+//       author: "Mosh",
+//       isPublished: false
+//     }
+//   }
+// );
+
+// Another Method
+
+// const course = await Course.findByIdAndUpdate(
+//   id,
+
+//   {
+//     $set: {
+//       author: "Jack",
+//       isPublished: true
+//     }
+//   },
+//   { new: true }
+// );
+// console.log(course);
+// }
+
+// updateCourse("5c5ab393feb3c80a348dbb20");
+
+// async function removeCourse(id) {
+//   const result = await Course.deleteOne({ _id: id });
+//   console.log(result);
+// }
+
+// removeCourse("5c5ab393feb3c80a348dbb20");
+
+async function removeCourse(id) {
+  const result = await Course.deleteMany({ _id: id });
+  console.log(result);
 }
 
-getCourses();
+removeCourse("5c5ab393feb3c80a348dbb20");
